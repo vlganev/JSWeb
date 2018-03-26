732dda364ca85a150d6c1bdcb9ce6cc6ac9712a3
Vladislav Ganev <vladislavganev@gmail.com>
2018-03-26 20:12:05 +0300

    ReactJS Routing and Architecture commit.

---
+++ ReactJS Fundamentals/Routing and Architecture/src/utils/dataCollector.js
@@ -0,0 +1,9 @@
+const appId = 'kid_S1KgSBYKz';
+const appSecret = '33e96aaec8cf48328739000aaf23922c';
+const url = 'https://baas.kinvey.com';
+
+export default {
+    appId,
+    appSecret,
+    url
+}
